c3bfe3b3ada021678c167e987a6cf3148829e520
lidong <lidong@zepplabs.com>
2018-07-13 09:58:55 +0800

    文件改名

--- udp_gui.py
+++ udp_gui.py
@@ -7,14 +7,12 @@ import datetime
 from socket import *
 import _thread
 
-
-global_var = {'meeting_status':0, 'old_time':0, 'new_time':0, 'time_str':'00:00:00', 'send_flag':False}
-server = {'IP':'127.0.0.1', 'PORT':9999}
+## global variable
+global_var = {'meeting_status':False, 'old_time':0, 'new_time':0, 'time_str':'00:00:00', 'send_flag':False}
+server = {'IP':'192.168.31.209', 'PORT':9999}
 
 def fun_udp_msg_send(udp_msg):  
-    msg_meeting_status.value = udp_msg
-    global_var['send_flag'] = True
-    pass
+    my_socket.sendall(udp_msg.encode())
 
 def fun_calculate_time_elapse():
     global_var['new_time'] = datetime.datetime.now()
@@ -30,32 +28,29 @@ def fun_meeting_control():
     global meeting_status # True is start, False is stop
     if global_var['meeting_status'] == False: 
         global_var['meeting_status'] = True
-        button_cmd.text = 'meeting stop'
-        button_cmd.bg = 'red'
         fun_udp_msg_send('start')
+        ## enable timer
         global_var['old_time'] = datetime.datetime.now()
         msg_time_elapse.repeat(1000, fun_calculate_time_elapse)
-        ## reset elapse time
+        ## update UI
+        button_cmd.text = 'meeting stop'
+        button_cmd.bg = 'red'
         global_var['time_str'] = '00:00:00'
         msg_time_elapse.value = global_var['time_str']        
     else:
+        fun_udp_msg_send('stop')
+        ## disable timer
+        msg_time_elapse.cancel(fun_calculate_time_elapse)
+        ## update UI
         global_var['meeting_status'] = False
         button_cmd.text = 'meeting start'
         button_cmd.bg = 'white'
-        fun_udp_msg_send('stop')
-        msg_time_elapse.cancel(fun_calculate_time_elapse)
+        
 
 def thread_udp_send():
     while True:
-        if global_var['send_flag'] == True: # True means msg is need to send, False means send is over.
-            my_socket.sendall(msg_meeting_status.value.encode())
-            global_var['send_flag'] = False
-            print ('true')
-        else:
-            print ('false')
-            time.sleep(1)
-        ##data = my_socket.recv(1024)
-        ##print(data)
+        recv_data = my_socket.recv(1024)
+        print(recv_data)
     my_socket.close()
 
 
@@ -63,19 +58,19 @@ def thread_udp_send():
 app = App(title = 'meeting console', width = 320, height = 240)
 
 ## create text
-msg_meeting_status = Text(app, text='stop')
 msg_time_elapse = Text(app, text='00:00:00')
 
-## creat button
+## create button
 button_cmd = PushButton(app, command = fun_meeting_control, text = 'meeting start')
 button_cmd.bg = 'white'
+button_cmd.height = 4
 
-
+## create UDP socket
 my_socket = socket(AF_INET,SOCK_DGRAM)
 my_socket.connect((server['IP'],server['PORT']))
-                             
-_thread.start_new_thread(thread_udp_send, ())
 
+## create UDP recv thread
+_thread.start_new_thread(thread_udp_send, ())
 
 
 ## full screen
