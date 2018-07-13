7b991dc1a5a6dbc1a27853d04ca6a00e039256a4
lidong <lidong@zepplabs.com>
2018-07-13 10:00:52 +0800

    修改按钮默认颜色为绿色 删除无效空格

--- udp_gui.py
+++ udp_gui.py
@@ -11,22 +11,23 @@ import _thread
 global_var = {'meeting_status':False, 'old_time':0, 'new_time':0, 'time_str':'00:00:00', 'send_flag':False}
 server = {'IP':'192.168.31.209', 'PORT':9999}
 
-def fun_udp_msg_send(udp_msg):  
+
+def fun_udp_msg_send(udp_msg):
     my_socket.sendall(udp_msg.encode())
 
 def fun_calculate_time_elapse():
     global_var['new_time'] = datetime.datetime.now()
     time_delta = global_var['new_time'] - global_var['old_time']
-    m, s = divmod(time_delta.seconds, 60) 
-    h, m = divmod(m, 60) 
+    m, s = divmod(time_delta.seconds, 60)
+    h, m = divmod(m, 60)
     global_var['time_str'] = '{0:02d}:{1:02d}:{2:02d}'.format(h, m, s)
     print (global_var['time_str'])
     msg_time_elapse.value = global_var['time_str']
-    
+
 
 def fun_meeting_control():
     global meeting_status # True is start, False is stop
-    if global_var['meeting_status'] == False: 
+    if global_var['meeting_status'] == False:
         global_var['meeting_status'] = True
         fun_udp_msg_send('start')
         ## enable timer
@@ -36,7 +37,7 @@ def fun_meeting_control():
         button_cmd.text = 'meeting stop'
         button_cmd.bg = 'red'
         global_var['time_str'] = '00:00:00'
-        msg_time_elapse.value = global_var['time_str']        
+        msg_time_elapse.value = global_var['time_str']
     else:
         fun_udp_msg_send('stop')
         ## disable timer
@@ -44,8 +45,8 @@ def fun_meeting_control():
         ## update UI
         global_var['meeting_status'] = False
         button_cmd.text = 'meeting start'
-        button_cmd.bg = 'white'
-        
+        button_cmd.bg = 'green'
+
 
 def thread_udp_send():
     while True:
@@ -62,7 +63,7 @@ msg_time_elapse = Text(app, text='00:00:00')
 
 ## create button
 button_cmd = PushButton(app, command = fun_meeting_control, text = 'meeting start')
-button_cmd.bg = 'white'
+button_cmd.bg = 'green'
 button_cmd.height = 4
 
 ## create UDP socket
@@ -74,7 +75,7 @@ _thread.start_new_thread(thread_udp_send, ())
 
 
 ## full screen
-##app.tk.attributes("-fullscreen",True)
+app.tk.attributes("-fullscreen",True)
 app.display()
 
 
