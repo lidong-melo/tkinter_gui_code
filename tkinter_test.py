85d23d4c399182bf4328e2d021d4b4d8b6d39178
lidong <lidong@zepplabs.com>
2018-07-20 17:47:51 +0800

    恢复之前实现的按钮计时逻辑 修正label angle的显示代码

--- tkinter_test.py
+++ tkinter_test.py
@@ -4,13 +4,14 @@
 
 import tkinter as tk
 from PIL import ImageTk, Image ## to use png format, import imageTK
-from tkinter import Button, Frame, PhotoImage,Label
+from tkinter import Button, PhotoImage,Label
 import platform
 from socket import *
 import _thread
 import random
 import time
 import json
+import datetime
 
 ## global variable
 global_var = {'meeting_status':False, 'old_time':0, 'new_time':0, 'time_str':'00:00:00', 'angle':0}
@@ -23,8 +24,7 @@ def rotate_pic(angle):
     global_var['angle'] = angle
     photo_bg.paste(image_bg.rotate(global_var['angle']))
     label_bg.config(image = photo_bg) 
-    label_time.text = str(angle)
-
+    label_angle.config(text = str(angle))
 
 
 def fun_udp_msg_send(udp_msg):
@@ -49,7 +49,6 @@ def thread_udp_recv():
                 global_var['meeting_status'] = recv_param['status']
             else:
                 print('wrong param')
-
             
             '''
             recv_str = recv_data.decode()
@@ -77,32 +76,40 @@ def thread_random_rotate():
         pass
         try:
             pass
-            #rotate_pic(angle)
+            rotate_pic(angle)
         except:
             pass
-        
-   
+      
+
+
+def thread_time_update():
+    while global_var['meeting_status'] == True:
+        global_var['new_time'] = datetime.datetime.now()
+        time_delta = global_var['new_time'] - global_var['old_time']
+        m, s = divmod(time_delta.seconds, 60)
+        h, m = divmod(m, 60)
+        global_var['time_str'] = '{0:02d}:{1:02d}:{2:02d}'.format(h, m, s)
+        print(global_var['time_str'])
+        label_time.config(text = global_var['time_str'])
+        time.sleep(1)
    
 
 def fun_meeting_control():
     global meeting_status # True is start, False is stop
     if global_var['meeting_status'] == False:
         global_var['meeting_status'] = True
-        fun_udp_msg_send('start')
-        ## enable timer
-        #    global_var['old_time'] = datetime.datetime.now()
-        #    msg_time_elapse.repeat(1000, fun_calculate_time_elapse)
-        ## update UI
+        fun_udp_msg_send('start') 
         photoimage_button.config(file='image_button_stop-1.png')
-        #    global_var['time_str'] = '00:00:00'
-        #    msg_time_elapse.value = global_var['time_str']
+        global_var['time_str'] = '00:00:00'
+        ## initial time
+        global_var['old_time'] = datetime.datetime.now()
+        global_var['new_time'] = global_var['old_time']        
+        _thread.start_new_thread(thread_time_update, ())
     else:
         fun_udp_msg_send('stop')
-        ## disable timer
-        #    msg_time_elapse.cancel(fun_calculate_time_elapse)
-        ## update UI
         global_var['meeting_status'] = False
         photoimage_button.config(file='image_metal_button-1.png')
+        
 
 
 
@@ -125,11 +132,10 @@ root.config(width = 320, height = 240)
 if(sysstr == "Linux"):
     root.attributes("-fullscreen", True)
     root.config(cursor="none")
-    server = {'IP':'10.0.5.1', 'PORT':9999}
+    ##server = {'IP':'10.0.5.1', 'PORT':9999}
 	
 
-label_time = Label(root)
-label_time.place(x=0,y=0)
+
 
 
 ## clock background
@@ -147,6 +153,13 @@ button_cmd.place(x=110,y=70)
 #button.config(image = 'image_metal_button.png')
 button_cmd.config(width = 100, height = 100, borderwidth = 0, highlightthickness = 0)
 
+label_angle = Label(root, font='Arial -20 bold')
+label_angle.place(x=0,y=0)
+
+label_time = Label(root, font='Arial -14 bold', text='00:00:00')
+label_time.place(x=0,y=220)
+
+
 
 ## create UDP socket
 my_udp_socket = socket(AF_INET,SOCK_DGRAM)
@@ -155,7 +168,10 @@ my_udp_socket.connect((server['IP'],server['PORT']))
 
 ## create UDP recv thread
 _thread.start_new_thread(thread_udp_recv, ())
-_thread.start_new_thread(thread_random_rotate, ())
+
+#_thread.start_new_thread(thread_random_rotate, ())
+
+
 
 root.mainloop()
 
