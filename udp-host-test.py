0b4aa9b15189acf46ac1a8e598c274da2180d96e
lidong <lidong@zepplabs.com>
2018-07-20 16:35:03 +0800

    修正代码，client更换端口也可以正常连接。

--- udp-host-test.py
+++ udp-host-test.py
@@ -7,28 +7,46 @@ from socket import *
 import json
 import random
 import time
+import _thread
   
 HOST = '192.168.31.209'  
 PORT = 60000
-
+address_list = ['192.168.31.209', 10000]
 param2 = {'msg_type':'t2r', 'doa_angle':0} 
 
+
+def thread_udp_recv():
+    while True:
+        print ('...waiting for message..'  )
+        try:
+            data,address_recv = s.recvfrom(1024)
+            print('recv',address_recv)
+            address_list.clear()
+            address_list_temp = list(address_recv)
+            address_list.append(address_list_temp[0])
+            address_list.append(address_list_temp[1])
+            print('recv',address_list)
+            
+        except:
+            pass
+
+
 s = socket(AF_INET,SOCK_DGRAM)  
-s.bind((HOST,PORT))  
+s.bind((HOST,PORT))
+_thread.start_new_thread(thread_udp_recv, ())
+
 
 while True:
-    print ('...waiting for message..'  )
-    data,address = s.recvfrom(1024)
-    while True:
-        sleep_time = random.uniform(0.5, 3)
-        time.sleep(sleep_time)
-        angle = random.randint(0,360)
-        param2['doa_angle'] = angle
-        json_string = json.dumps(param2)
-        s.sendto(json_string.encode(), address)
-        try:
-            s.sendto(json_string.encode(), address)
-            print ('send', json_string)
-        except:
-            break
+
+    sleep_time = random.uniform(0.5, 3)
+    time.sleep(sleep_time)
+    angle = random.randint(0,360)
+    param2['doa_angle'] = angle
+    json_string = json.dumps(param2)
+    address_tuple = tuple(address_list)
+    try:
+        s.sendto(json_string.encode(), address_tuple)
+        print('send', address_tuple, json_string)
+    except:
+        print('disconnect')
 s.close()
