0f79cfa9bdd04cd1a45d329f9de42e8978b06fcd
DESKTOP-C3FOGJH\lidon <lidong@zepplab.com>
2018-08-06 13:48:37 +0800

    实现tx2端 rssi的获取

---
+++ tx2/get_rssi.py
@@ -0,0 +1,33 @@
+import subprocess
+import re
+import time
+
+#proc = subprocess.Popen(["iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True)
+#sudo iw dev wlan0 scan | grep -E 'on wlan0|signal:|SSID:' > ~/lidong/rssi.txt
+
+def get_rssi():
+    proc = subprocess.Popen(["iw", "wlan0", "scan"], stdout=subprocess.PIPE, universal_newlines=True)
+    out, err = proc.communicate()
+    
+    index1 = out.find('associated')
+    index2 = out.find('signal', index1)
+    index3 = out.find('\n', index2)
+    ret = re.search('\d+', out[index2:index3]).group()
+    if ret:
+        print(ret)
+        return ret  
+    else:
+        print('no connection')
+        return 'error_code = 55'
+        
+        
+
+        
+while 1:
+    get_rssi()
+    time.sleep(10)
+    
+    
+    
+    
+    
